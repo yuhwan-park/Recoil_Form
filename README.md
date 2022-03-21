b16a3d56296cb795933715d70d328d58626f6125
yuhwan-park <apkdkvkz@gmail.com>
2022-03-21 17:54:35 +0900

    second commit

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 <ul>
 <li>first commit : useForm 훅을 이용하여 다수의 Input 제어</li>
+<li>second commit : React-hook-form의 기초적인 사용법 익힘, 에러처리까지 완료 스타일은 안할 예정</li>
 </ul>
--- src/Form.tsx
+++ src/Form.tsx
@@ -11,50 +11,96 @@ const Container = styled.div`
 const SignUpForm = styled.form`
   display: flex;
   flex-direction: column;
-  width: 300px;
+  width: 500px;
   input {
     margin: 10px;
   }
+  span {
+    font-size: 10px;
+    position: relative;
+    left: 8px;
+    color: red;
+    margin-bottom: 5px;
+  }
+  label {
+    color: white;
+    font-size: 12px;
+  }
+  button {
+    border: none;
+    background-color: powderblue;
+    color: white;
+    font-weight: 600;
+  }
 `;
+interface IForm {
+  name: string;
+  password: string;
+  password_confirm: string;
+  phone: string;
+}
 
 function Form() {
   // register = 인풋안에 넣음으로 인해 onSubmit 설정과 setState 할 필요가 없어짐
-  const { register, handleSubmit, formState } = useForm();
-  const onValid = (data: any) => {};
-  console.log(formState.errors, formState.submitCount);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>();
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password_confirm) {
+      setError(
+        "password_confirm",
+        { message: "비밀번호가 일치하지 않습니다" },
+        { shouldFocus: true }
+      );
+    }
+  };
   return (
     <Container>
       <SignUpForm onSubmit={handleSubmit(onValid)}>
+        <label htmlFor="name">이름</label>
         <input
           {...register("name", { required: "필수 항목입니다!" })}
           type="text"
-          placeholder="name"
+          id="name"
         />
+        <span>{errors?.name?.message}</span>
+        <label htmlFor="password">비밀번호</label>
         <input
-          {...register("password", { required: "필수 항목입니다!" })}
+          {...register("password", {
+            required: "필수 항목입니다!",
+            minLength: {
+              value: 8,
+              message: "비밀번호는 8자 이상 입력해주세요",
+            },
+          })}
           type="text"
-          placeholder="password"
+          id="password"
         />
+        <span>{errors?.password?.message}</span>
+        <label htmlFor="password">비밀번호 확인</label>
         <input
-          {...register("password_comfirm", { required: "필수 항목입니다!" })}
+          {...register("password_confirm", { required: "필수 항목입니다!" })}
           type="text"
-          placeholder="password_comfirm"
+          id="password_confirm"
         />
+        <span>{errors?.password_confirm?.message}</span>
+        <label htmlFor="phone">휴대폰 번호</label>
         <input
-          {...register("phone", { required: "필수 항목입니다!" })}
+          {...register("phone", {
+            required: "필수 항목입니다!",
+            pattern: {
+              value: /010\d{8,8}/,
+              message:
+                "010으로 시작하는 휴대폰 번호를 하이픈(-) 없이 적어주세요. (총 11자리)",
+            },
+          })}
           type="text"
-          placeholder="phone"
+          id="phone"
         />
-        <input
-          {...register("first_resident_num", { required: "필수 항목입니다!" })}
-          type="text"
-          placeholder="first_resident_num"
-        />
-        <input
-          {...register("second_resident_num", { required: "필수 항목입니다!" })}
-          type="text"
-          placeholder="second_resident_num"
-        />
+        <span>{errors?.phone?.message}</span>
         <button>Sign Up</button>
       </SignUpForm>
     </Container>
